f03ed9ec543e4ee543ccb6f63d36e1b6061b2fc5
Guixin Zhu <guixin.zhu@yahoo.com>
2025-02-19 23:30:26 +0900

    make direct route to static files possible

---
+++ back/src/main/java/com/example/demo/HtmlController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+@Controller
+public class HtmlController {
+
+    @RequestMapping("/{page:^(?!.*[.].*$).*$}")
+    public String requestMethodName(@PathVariable("page") String page) {
+        String htmlPage = "/"+page+".html";
+        log.info("forwarding request to {}", htmlPage);
+        return htmlPage;
+    }
+    
+}
