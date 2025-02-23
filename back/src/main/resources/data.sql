081db19b49363e42ea0799857ffbe9cbf1e5a7e6
Guixin Zhu <guixin.zhu@yahoo.com>
2025-02-23 23:13:55 +0900

    use keycloak - applicaiton.yml

--- back/src/main/resources/data.sql
+++ back/src/main/resources/data.sql
@@ -1,3 +1,8 @@
 insert into cash_posting values ('2024-12-11', 'Y');
 insert into cash_posting values ('2024-12-12', 'Y');
 insert into cash_posting values ('2024-12-13', 'N');
+
+
+insert into cash_posting values ('2025-02-19', 'Y');
+insert into cash_posting values ('2025-02-20', 'Y');
+insert into cash_posting values ('2025-02-21', 'N');
