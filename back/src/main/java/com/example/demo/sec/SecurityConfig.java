1c364fb3fd50befd25521bbc39abb20e38c1a689
Guixin Zhu <guixin.zhu@yahoo.com>
2025-02-24 01:21:44 +0900

    disable crsf for now

--- back/src/main/java/com/example/demo/sec/SecurityConfig.java
+++ back/src/main/java/com/example/demo/sec/SecurityConfig.java
@@ -13,7 +13,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(auth -> auth
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/css/**", "/js/**", "/images/**").permitAll()
                 .anyRequest().authenticated())
                 .oauth2Login(Customizer.withDefaults())
